3ac4fa4b9491ccedd019f4ff44126d61e371ea6d
AlecUrbany <98421122+AlecUrbany@users.noreply.github.com>
2022-02-27 22:03:52 -0800

    Update ArrayBagTest.java

--- Project 1/ArrayBagTest.java
+++ Project 1/ArrayBagTest.java
@@ -5,22 +5,22 @@ public class ArrayBagTest
 
 	public static void main(String[] args) 
 	{
-		ResizableArrayBag<String> bag1 = new ResizableArrayBag<String>(3);
+		ResizeableArrayBag<String> bag1 = new ResizeableArrayBag<String>(3);
 		bag1.add("a");
 		bag1.add("b");
 		bag1.add("c");
 		System.out.println("bag1: " + Arrays.toString(bag1.toArray()));
 
-		ResizableArrayBag<String> bag2 = new ResizableArrayBag<String>(4);
+		ResizeableArrayBag<String> bag2 = new ResizeableArrayBag<String>(4);
 		bag2.add("b");
 		bag2.add("b");
 		bag2.add("d");
 		bag2.add("e");
 		System.out.println("bag2: " + Arrays.toString(bag2.toArray()));
 		
+		System.out.print("Union is: " + Arrays.toString(bag1.union(bag2).toArray()) + " ");
+		System.out.print("Intersection is: " + Arrays.toString(bag1.intersection(bag2).toArray()) + " ");
+		System.out.print("Difference is: " + Arrays.toString(bag1.difference(bag2).toArray()) + " ");
 	}
 
-	System.out.print(bag1.union(bag2));
-
-	
 }

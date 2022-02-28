1264771e720fdc947710ef9a9948a2dd1568ff3d
AlecUrbany <98421122+AlecUrbany@users.noreply.github.com>
2022-02-27 22:16:12 -0800

    Update LinkedBagTest.java

--- Project 1/LinkedBagTest.java
+++ Project 1/LinkedBagTest.java
@@ -1,24 +1,22 @@
 import java.util.Arrays;
 
-public class LinkedBagTest 
+public class LinkedBagTest
 {
 
 	public static void main(String[] args) 
-	{
-		LinkedBag<String> bag1 = new LinkedBag<String>();
-		bag1.add("a");
-		bag1.add("b");
-		bag1.add("c");
+	{	
+		ResizeableArrayBag<String> bag1 = new ResizeableArrayBag<String>(3);
+		bag1.add("apples");
+		bag1.add("bananas");
+		bag1.add("cream");
 		System.out.println("bag1: " + Arrays.toString(bag1.toArray()));
-		
-		LinkedBag<String> bag2 = new LinkedBag<String>();
-		bag2.add("b");
-		bag2.add("b");
-		bag2.add("d");
-		bag2.add("e");
-		System.out.println("bag2: " + Arrays.toString(bag2.toArray()));
-		
 
+		LinkedBag<String> bag2 = new LinkedBag<String>();
+		bag2.add("bananas");
+		bag2.add("bananas");
+		bag2.add("doom");
+		bag2.add("extra cheese");
+		System.out.print("\nUnion is: " + Arrays.toString(bag2.union(bag1).toArray()) + " ");
 	}
 
 }
